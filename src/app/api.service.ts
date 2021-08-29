fd9766b383aaff32f4dbea31a69f1552e7eb7460
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-29 18:15:49 +0300

    adds an API linkng the app to the github on the service file

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from '../environments/environment'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  user:any;
+  repos:any;
+
+  constructor( private http:HttpClient) { }
+  
+  showUser(user:any){
+    return this.http.get("https://api.github.com/users/" + user+ "?access_token=" + environment.gitApi)
+    .pipe(((response:any)=>response));
+  }
+
+  // users(){
+   
+  
 }
