3e788588b55dc4600d32758cdf114623ec5da596
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-29 22:45:35 +0300

    Adds routes to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from 'src/app/profile/profile.component'
+import { RepositoriesComponent } from 'src/app/repositories/repositories.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profiles', component: ProfileComponent},
+  {path: 'repositories', component: RepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
