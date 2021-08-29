b2637869096af07a1fb082270088861b492d3529
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-29 18:12:36 +0300

    adds a profiles constructor

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service'
+
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css']
+  styleUrls: ['./github.component.css'],
+  
 })
+
+// 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
+
 export class GithubComponent implements OnInit {
 
-  constructor() { }
+  user:any;
+  displayUser:any;
+  showUserName(){
+    this.service.showUser(this.user).subscribe(profile => {
+      console.log(profile)
+      return this.displayUser = profile;
+    });
+  }
+
+  constructor( private service:ApiService ) {  }
 
   ngOnInit(): void {
   }
