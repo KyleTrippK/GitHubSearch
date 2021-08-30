0f803b4055d3e913dd2da12c4bff1cbba764a411
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-30 03:53:53 +0300

    modifies the routing of the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from 'src/app/profile/profile.component'
 import { RepositoriesComponent } from 'src/app/repositories/repositories.component'
-import { NotFoundComponent } from 'src/app/not-found/not-found.component'
+//import { NotFoundComponent } from 'src/app/not-found/not-found.component'
+// import { GithubComponent } from './github/github.component';
 
 
 const routes: Routes = [
   {path: 'profiles', component: ProfileComponent},
   {path: 'repositories', component: RepositoriesComponent},
-  {path: '**', component: NotFoundComponent},
+  //{path: '**', component: NotFoundComponent},
+  {path: '',  pathMatch:'prefix', redirectTo:'/profiles'},
 ];
 
 @NgModule({
