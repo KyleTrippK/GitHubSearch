f842a0be2b314e19e4a2716bb6327f5d51f42a7b
KyleTrippk <kyle.kibet@student.moringaschool.com>
2021-08-30 08:35:05 +0300

    polishes the page

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/api.service'
-
+import { ApiService } from 'src/app/api.service';
+import { ReposerveService } from 'src/app/reposerve.service'
 
 @Component({
   selector: 'app-github',
   templateUrl: './github.component.html',
-  styleUrls: ['./github.component.css'],
-  
+  styleUrls: ['./github.component.css'],  
 })
 
 
@@ -14,13 +13,20 @@ export class GithubComponent implements OnInit {
 
   user:any;
   displayUser:any;
+  shiwUse:any;
   showUserName(){
-    this.service.showUser(this.user).subscribe(profile => {
-      console.log(profile)
-      return this.displayUser = profile;
+    this.service.showUser(this.user).subscribe(about => {
+      console.log(about)
+      return this.displayUser = about;
     });
   }
-  constructor( private service:ApiService ) {  }
+  showUse(){
+    this.newserve.getRepo(this.user).subscribe(description => {
+      console.log(description)
+      return this.shiwUse = description;
+    });
+  }
+  constructor( private service:ApiService, private newserve:ReposerveService ) {  }
 
   ngOnInit(): void {
   }
